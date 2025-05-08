28f620455e2a10b45234ed713141bf152cc5b41b
Lea Devoudelle-Royer <ldevoude@z4r4p5.42lyon.fr>
2025-05-08 15:14:23 +0200

    {misc} commit to save before some heavy modifications.

--- srcs/exec/child.c
+++ srcs/exec/child.c
@@ -6,54 +6,13 @@
 /*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 08:22:44 by ldevoude          #+#    #+#             */
-/*   Updated: 2025/05/07 16:53:02 by ldevoude         ###   ########lyon.fr   */
+/*   Updated: 2025/05/08 07:22:51 by ldevoude         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/exec.h"
 
-// TODO also need a struct with env + last return
-void	child_maker(t_command *node, int number_nodes)
-{
-	int		i;
-	int		pipe_fd[2];
-	int		previous_pipe;
-	pid_t	child;
 
-	previous_pipe = -1;
-	while (node)
-	{
-		if (i < number_nodes - 1)
-			pipe(pipe_fd); // TODO protect
-		child = fork();    // protect
-		if (child == 0)
-		{
-			if (previous_pipe != -1)
-			{
-				dup2(previous_pipe, STDIN_FILENO);
-				close(previous_pipe);
-			}
-			else if (node->redirect_in)
-				// TODO FUNCTION FOR THAT CASE WHEN REDIRECTIN
-				if (pipe_fd[0])
-					close(pipe_fd[0]);
-			// COMPLETE PATH+ARG+EXECVE
-			exit(1); // FAIL IF EXECVE DOESNT WORK
-		}
-		else
-		{
-			// parent close what we dont need anymore
-			if (previous_pipe != -1)
-				close(previous_pipe);
-			if (i < number_nodes - 1)
-				close(pipe_fd[1]);
-			previous_pipe = pipe_fd[0];
-			node = node->next;
-			i++;
-		}
-	}
-	// TODO WAIT CHILDS WAITPID
-}
 // exec goes to 2 different path at the moment. first one
 // is when there is only 1 cmd that is a built the 2nd one
 // is whenever the first condition isnt fullfilled
--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 16:00:29 by ldevoude          #+#    #+#             */
-/*   Updated: 2025/05/07 08:22:21 by ldevoude         ###   ########lyon.fr   */
+/*   Updated: 2025/05/08 11:36:19 by ldevoude         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,57 @@
 
 #include "../include/exec.h"
 
+
 // check if it is a built-in cmd
+// TODO also need a struct with env + last return
+
+void	init_child(t_command *node, int *pipe_fd, int previous_pipe)
+{
+	if (node->redirect_in)
+		printf("Ok\n");// TODO FUNCTION FOR THAT CASE WHEN REDIRECTIN
+	else if (previous_pipe != -1)
+		{
+			dup2(previous_pipe, STDIN_FILENO);
+			close(previous_pipe);
+		}
+	if (previous_pipe == -1)
+		close(pipe_fd[0]);
+	// COMPLETE PATH+ARG+EXECVE
+	exit(1); // FAIL IF EXECVE DOESNT WORK
+}
+
+void	child_maker(t_command *node, int number_nodes)
+{
+	int		i;
+	int		pipe_fd[2];
+	int		previous_pipe;
+	pid_t	child;
+
+	i = 0;
+	previous_pipe = -1; //TODO #define NO_PREV_PIPE
+	while (node)
+	{
+		if (i < number_nodes - 1)
+			pipe(pipe_fd); // TODO protect
+		child = fork();    // protect
+		if (child == 0)
+			init_child(node, pipe_fd, previous_pipe);
+		else
+		{
+			// parent close what we dont need anymore
+			
+			if (i < number_nodes - 1)
+				close(pipe_fd[1]);
+			if (previous_pipe != -1)
+				close(previous_pipe);
+			previous_pipe = pipe_fd[0];
+			close(pipe_fd[0]);
+			node = node->next;
+			i++;
+		}
+	}
+	// TODO WAIT CHILDS WAITPID
+}
 
 bool	built_in_checker(char *cmd)
 {
