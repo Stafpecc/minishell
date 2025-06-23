0061c448810f365513a97a73cf9e8402742064c2
Stafpec <theoarini000@gmail.com>
2025-06-23 12:12:01 +0200

    fix all unsecure malloc except fill

--- include/minishell.h
+++ include/minishell.h
@@ -13,6 +13,9 @@
 #include "signal_handler.h"
 #include "../libft/includes/libft.h"
 
+#define RED "\033[0;31m"
+#define RESET "\033[0m"
+
 void print_commands(t_command *cmd); // DEBUG
 void print_tokens(t_token *head); // DEBUG
 void print_command_exec(t_command_exec *cmd); // DEBUG
