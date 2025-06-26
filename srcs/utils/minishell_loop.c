eb99aa9ed8ca1ebe68a08d1d8986f4f66c0844d8
Stafpec <theoarini000@gmail.com>
2025-06-26 16:21:30 +0200

    fix lastreturnonctrlk+d(minishell_loop.c): add exit builtin on ctrld

--- srcs/utils/minishell_loop.c
+++ srcs/utils/minishell_loop.c
@@ -6,7 +6,7 @@
 /*   By: stafpec <stafpec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 18:29:16 by stafpec           #+#    #+#             */
-/*   Updated: 2025/06/26 12:53:46 by stafpec          ###   ########.fr       */
+/*   Updated: 2025/06/26 16:15:43 by stafpec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,27 +33,14 @@ static bool	skip_empty_or_spaces(char *input)
 	return (false);
 }
 
-static bool	handle_null_input(char *input)
+static bool	handle_null_input(char *input, t_utils *utils)
 {
 	if (!input)
 	{
-		write(1, "exit\n", 5);
+		exit_builtin(NULL, utils);
 		return (false);
 	}
 	return (true);
-}
-
-static bool	handle_exit_builtin(char *input, t_token *token, t_utils *utils)
-{
-	if (ft_strcmp(input, "exit") == 0)
-	{
-		exit_proprely(3,
-			(void (*)(void *))free, input,
-			(void (*)(void *))free_tokens, token,
-			(void (*)(void *))free_utils, utils);
-		return (true);
-	}
-	return (false);
 }
 
 void	minishell_loop(t_utils *utils)
@@ -65,7 +52,7 @@ void	minishell_loop(t_utils *utils)
 	while (1)
 	{
 		input = read_input_with_quotes();
-		if (!handle_null_input(input))
+		if (!handle_null_input(input, utils))
 			exit_proprely(0);
 		if (skip_empty_or_spaces(input))
 			continue ;
@@ -74,8 +61,6 @@ void	minishell_loop(t_utils *utils)
 		token = process_lexer(input, utils);
 		if (!token)
 			continue ;
-		if (handle_exit_builtin(input, token, utils))
-			return ;
 		utils->type_of_first_arg = token->type;
 		command = parse_tokens(token, utils);
 		execute_or_cleanup(command, token, input, utils);
