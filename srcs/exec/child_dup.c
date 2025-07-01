0b23608ff464c948a9fefde401410912fe0911ab
Lea Devoudelle-Royer <ldevoude@z4r6p4.42lyon.fr>
2025-07-01 11:55:34 +0200

    norme(child_dup_utils.c): norme compatible feat(child_dup_utils.c): now child_dup has been divided into two files for norme and readability reasons

--- srcs/exec/child_dup.c
+++ srcs/exec/child_dup.c
@@ -6,102 +6,20 @@
 /*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 14:55:02 by ldevoude          #+#    #+#             */
-/*   Updated: 2025/06/26 15:25:12 by ldevoude         ###   ########lyon.fr   */
+/*   Updated: 2025/07/01 10:47:27 by ldevoude         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/exec.h"
 
-int redirect_write_browser(t_redirect **redirect, int i)
-{
-
-	while (redirect[i])
-		{
-
-			if(redirect[i]->append_redirect == FALSE)
-			{
-				redirect[i]->fd = open(redirect[i]->arg, O_CREAT | O_WRONLY | O_TRUNC, 0644); //secure
-			}
-			else
-				redirect[i]->fd = open(redirect[i]->arg, O_CREAT | O_WRONLY | O_APPEND, 0644); //secure
-			if (redirect[i]->fd < 0)
-				return (RETURN_FAILURE);
-			if (dup2(redirect[i]->fd, STDOUT_FILENO) < 0)
-			{
-				close(redirect[i]->fd);
-				return (RETURN_FAILURE);
-			}
-			if(close(redirect[i]->fd) == -1) //TORM IF WORKING
-				return(RETURN_FAILURE); 
-			i++;
-		}
-	return (RETURN_SUCCESS);
-}
-
-int redirect_read_browser(t_redirect **redirect, int i)
-{
-	while (redirect[i])
-		{
-			if(redirect[i]->heredoc == FALSE)
-				redirect[i]->fd = open(redirect[i]->arg, O_RDONLY); 
-			if (redirect[i]->fd < 0)
-				return (RETURN_FAILURE);
-			if (dup2(redirect[i]->fd, STDIN_FILENO) < 0)
-			{
-				close(redirect[i]->fd);
-				return (RETURN_FAILURE);
-			}
-			if(close(redirect[i]->fd) == -1) //TORM IF WORKING
-				return (RETURN_FAILURE);
-			i++;
-		}
-	return (RETURN_SUCCESS);
-}
-
-int	write_dup(t_redirect **redirect, int *pipe_fd)
-{
-	if(redirect)
-	{
-		if(redirect_write_browser(redirect, 0))
-			return(RETURN_FAILURE);
-	}
-	else if (pipe_fd[1] != -42 && !redirect)
-	{
-		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-			return (2);
-	}
-	return (RETURN_SUCCESS);
-}
-
-// dup the right fd for stdinput and return
-// an error code if it doesnt work properly
-int	read_dup(t_redirect **redirect, /*int *pipe_fd,*/ int previous_pipe)
-{
-	if(redirect)
-	{
-		if(redirect_read_browser(redirect, 0))
-			return (RETURN_FAILURE);
-	}	
-	else if (previous_pipe != NONE)
-	{
-		if (dup2(previous_pipe, STDIN_FILENO) == -1)
-			return (RETURN_FAILURE);
-	}
-	// else if (previous_pipe == NONE)
-	// {
-	// 	if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
-	// 		return (RETURN_FAILURE);
-	// }
-	return (RETURN_SUCCESS);
-}
-
 // Used if we do only have one cmd
 //TODO complete doc about that function
+//TORM read up same as child_init
 void only_child(t_command_exec *node, int *pipe_fd, t_utils *utils)
 {
 
 	if(node->redirect_in
-		&& read_dup(node->redirect_in, /*pipe_fd,*/ utils->previous_pipes))
+		&& read_dup(node->redirect_in, utils->previous_pipes))
 	{
 		close(pipe_fd[0]);
 		close(pipe_fd[1]);
@@ -125,12 +43,14 @@ void only_child(t_command_exec *node, int *pipe_fd, t_utils *utils)
 // would dup2 the right fdsexec went well
 // it will close our fds then we go to the
 // next part of the process
-//TODO when new struct created, update all of it :> (previous pipe, num_nodes)
+//TORM line 134 = if (read_dup(node->redirect_in
+//, pipe_fd, utils->previous_pipes)) if no work
+
 void	child_init_pipes_dup(t_command_exec *node, int *pipe_fd, t_utils *utils)
 {
 	if(utils->num_nodes == 1)
 		only_child(node, pipe_fd, utils);
-	if (read_dup(node->redirect_in, /*pipe_fd,*/ utils->previous_pipes))
+	if (read_dup(node->redirect_in, utils->previous_pipes))
 	{
 		if (utils->previous_pipes != NONE)
 			close(utils->previous_pipes);
---
+++ srcs/exec/child_dup_utils.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   child_dup_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 10:44:59 by ldevoude          #+#    #+#             */
+/*   Updated: 2025/07/01 11:03:21 by ldevoude         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+// if(close(redirect[i]->fd) == -1) //TORM IF WORKING
+// return(RETURN_FAILURE);
+
+#include "../../include/exec.h"
+
+int	redirect_write_browser(t_redirect **redirect, int i)
+{
+	while (redirect[i])
+	{
+		if (redirect[i]->append_redirect == FALSE)
+		{
+			redirect[i]->fd = open(redirect[i]->arg,
+					O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		}
+		else
+			redirect[i]->fd = open(redirect[i]->arg,
+					O_CREAT | O_WRONLY | O_APPEND, 0644);
+		if (redirect[i]->fd < 0)
+			return (RETURN_FAILURE);
+		if (dup2(redirect[i]->fd, STDOUT_FILENO) < 0)
+		{
+			close(redirect[i]->fd);
+			return (RETURN_FAILURE);
+		}
+		if (close(redirect[i]->fd) == -1)
+			return (RETURN_FAILURE);
+		i++;
+	}
+	return (RETURN_SUCCESS);
+}
+
+// if(close(redirect[i]->fd) == -1) //TORM IF WORKING
+// return(RETURN_FAILURE);
+
+int	redirect_read_browser(t_redirect **redirect, int i)
+{
+	while (redirect[i])
+	{
+		if (redirect[i]->heredoc == FALSE)
+			redirect[i]->fd = open(redirect[i]->arg, O_RDONLY);
+		if (redirect[i]->fd < 0)
+			return (RETURN_FAILURE);
+		if (dup2(redirect[i]->fd, STDIN_FILENO) < 0)
+		{
+			close(redirect[i]->fd);
+			return (RETURN_FAILURE);
+		}
+		if (close(redirect[i]->fd) == -1)
+			return (RETURN_FAILURE);
+		i++;
+	}
+	return (RETURN_SUCCESS);
+}
+
+int	write_dup(t_redirect **redirect, int *pipe_fd)
+{
+	if (redirect)
+	{
+		if (redirect_write_browser(redirect, 0))
+			return (RETURN_FAILURE);
+	}
+	else if (pipe_fd[1] != -42)
+	{
+		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+			return (2);
+	}
+	return (RETURN_SUCCESS);
+}
+
+// dup the right fd for stdinput and return
+// an error code if it doesnt work properly
+// TORM read_dup same as bellow
+// else if (previous_pipe == NONE)
+// {
+// if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
+// return (RETURN_FAILURE);
+// }
+int	read_dup(t_redirect **redirect, int previous_pipe)
+{
+	if (redirect)
+	{
+		if (redirect_read_browser(redirect, 0))
+			return (RETURN_FAILURE);
+	}
+	else if (previous_pipe != NONE)
+	{
+		if (dup2(previous_pipe, STDIN_FILENO) == -1)
+			return (RETURN_FAILURE);
+	}
+	return (RETURN_SUCCESS);
+}
