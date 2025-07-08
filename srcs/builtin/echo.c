d9351509b2225adb042e9b9d44a6f6055180add6
ruban-allarkpet <ruban@allarkpet>
2025-07-08 19:17:37 +0200

    feat(echo.c): now echo ignore -n flags that follow up a valid one

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -11,34 +11,63 @@
 /* ************************************************************************** */
 
 #include "../include/builtin.h"
+#include "parsing.h"
+#include "return_error.h"
+
+#define NO_FLAG_N 1
+#define FLAG_N 0
+//here we check if there is any -n flag that follow
+//the first one that has been validated in is_newline
+//it updated index_print if it found a new valid -n 
+//array of chars
+static bool pass_n_flags(t_command_exec *node, int *index_print, int i)
+{
+	if (!node->cmd_parts[*index_print])
+		return(RETURN_SUCCESS);
+	while (node->cmd_parts[*index_print] && ft_strncmp(node->cmd_parts[*index_print], "-n", 2) == 0 )
+		{
+			while(node->cmd_parts[*index_print][i])
+			{
+				if (node->cmd_parts[1][i] == 'n')
+					i++;
+				else 
+				{
+					return(RETURN_SUCCESS);
+				}
+			}
+			i = 2;
+			*index_print += 1;
+		}
+	return(RETURN_SUCCESS);
+}
 
 // to check if we got a right parameters
-//+ if we need a new line or not
-static int	is_newline(t_command_exec *node, int i, bool *newline, int *index_print)
+// update the ptr newline if we need a new line or not
+// and index_print depending of the situation
+// if there was a valid n parameter then we also
+// return pass_n_flag to ignore all n flags that follow up
+static bool	is_newline(t_command_exec *node, int i, bool *newline, int *index_print)
 {
+	
 	if (!node->cmd_parts || !node->cmd_parts[0])
-		return (1);
+		return (RETURN_FAILURE);
 	if (node->cmd_parts[1] && ft_strncmp(node->cmd_parts[1], "-n", 2) == 0 )
 	{
 		*newline = FALSE;
 		*index_print = 2;
 		while(node->cmd_parts[1][i])
-		{
-			printf("chier dans les lasagnes\n");
-			if(!node->cmd_parts[1][i])
-				break;
+		{		
 			if (node->cmd_parts[1][i] == 'n')
 				i += 1;
 			else 
 			{
 				*index_print = 1;
 				*newline = TRUE;
-				break;
+				return(RETURN_SUCCESS);
 			}
 		}
 	}
-	//TODO a checker to see if there is more tag -n after that one
-	return (0);
+	return(pass_n_flags(node, index_print, 2));
 }
 
 // check if we received the right cmd and
